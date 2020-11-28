92a10f453fcac787c53db96c219bb6bcf43a33e0
Alexandre Moutinho <ac_system@outlook.com>
2020-11-28 01:42:39 -0300

    Finalizado ao 12 Inicio Aula 13

--- ConexaoBD/Appstart.cs
+++ ConexaoBD/Appstart.cs
@@ -16,6 +16,8 @@ namespace ConexaoBD
             Console.WriteLine("-------------< Escolha as Opções abaixo >----------- ");
             Console.WriteLine("| Opção 01 - Lista de Usuarios                      |");
             Console.WriteLine("| Opção 02 - Alterar  Usuarios                      |");
+            Console.WriteLine("| Opção 03 - Deletar  Usuarios                      |");
+            Console.WriteLine("| Opção 04 - Inserir  Usuarios                      |");
             Console.WriteLine("| Opção 05 - Sair Para O Menu Principal             |");
 
             Console.Write("Opção: ==>  ");
@@ -30,6 +32,12 @@ namespace ConexaoBD
                     Updade();
                     break;
 
+                case 3:
+                    Delete();
+                    break;
+                case 4:
+                    Insert();
+                    break;
                 case 5:
                     Menu menu = new Menu();
                     Menu.MenuPrincipal();
@@ -39,6 +47,29 @@ namespace ConexaoBD
                     Qcurso();
                     break;
             }
+
+
+            void Insert()
+            {
+                Console.Write("Nome: ");
+                string Nome = Console.ReadLine();
+                Console.Write("Cargo: ");
+                string Cargo = Console.ReadLine();
+                Console.Write("Data: ");
+                DateTime Data = DateTime.Parse(Console.ReadLine());
+                
+                string updade = $"INSERT INTO  tb_Usuario (Nome,Cargo,Data) Values ('{Nome}','{Cargo}','{Data}')  ";
+                SqlCommand cmd = new SqlCommand(updade, conexao.Qcurso);
+                SqlDataReader modificar = cmd.ExecuteReader();
+
+
+
+                while (modificar.Read())
+                {
+                    System.Console.WriteLine("UsuarioID {0} | Nome {1} | Cargo {2} | Data {3}\n", modificar["UsuarioId"], modificar["Nome"], modificar["Cargo"], modificar["Data"]);
+                }
+            }
+
 
 
 
@@ -60,15 +91,17 @@ namespace ConexaoBD
 
             void SelectView_ID(int id)
             {
-                string Select = $"SELECT UsuarioId  FROM tb_Usuario Where UsuarioId in {id}";
+                string Select = $"SELECT UsuarioId, Nome, Cargo, Data" +
+                    $"  FROM tb_Usuario Where UsuarioId = {id}";
                 SqlCommand cmdSelect = new SqlCommand(Select, conexao.Qcurso);
                 SqlDataReader view = cmdSelect.ExecuteReader();
-
+                
                 while (view.Read())
                 {
                     System.Console.WriteLine("UsuarioID {0} | Nome {1} | Cargo {2} | Data {3}\n", view["UsuarioId"], view["Nome"], view["Cargo"], view["Data"]);
                 }
-                               
+
+                view.Close();
             }
 
 
@@ -82,12 +115,34 @@ namespace ConexaoBD
                 Console.WriteLine("Entra com o Novo Nome");
                 string altnew = Console.ReadLine();
 
-                string updade = $"UPDATE  tb_Usuario SET Nome = '{altnew}' Where UsuarioId ={id}  ";
+                string updade = $"UPDATE  tb_Usuario SET Nome = '{altnew}' Where UsuarioId = {id}  ";
                 SqlCommand cmd = new SqlCommand(updade, conexao.Qcurso);
                 SqlDataReader modificar = cmd.ExecuteReader();
 
                 
                 
+                while (modificar.Read())
+                {
+                    System.Console.WriteLine("UsuarioID {0} | Nome {1} | Cargo {2} | Data {3}\n", modificar["UsuarioId"], modificar["Nome"], modificar["Cargo"], modificar["Data"]);
+                }
+            }
+
+            void Delete()
+            {
+                Console.WriteLine("Infome Id do  Nome que Desenha Deletar");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("---------------------------------------");
+                SelectView_ID(id);
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("Confirme o ID para Exluir");
+                int altnew = int.Parse(Console.ReadLine());
+
+                string delete = $"DELETE FROM tb_Usuario Where UsuarioId = {altnew}  ";
+                SqlCommand cmd = new SqlCommand(delete, conexao.Qcurso);
+                SqlDataReader modificar = cmd.ExecuteReader();
+
+
+
                 while (modificar.Read())
                 {
                     System.Console.WriteLine("UsuarioID {0} | Nome {1} | Cargo {2} | Data {3}\n", modificar["UsuarioId"], modificar["Nome"], modificar["Cargo"], modificar["Data"]);
--- ConexaoBD/ConexaoBD.cs
+++ ConexaoBD/ConexaoBD.cs
@@ -1,13 +1,27 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 
 namespace ConexaoBD
 {
-    class ConexaoBD
+    class ConexaoBD : IDisposable
     {
+        private readonly SqlConnection Conexao;
+        public ConexaoBD() { 
+        
+            Conexao= new SqlConnection(@"Data Source=ACSERVER;Initial Catalog=QCurso;Persist Security Info=True;User ID=sa;Password=apcofo@21");
+            Conexao.Open();
 
-        // //Conexão com Banco de Dados de QCursos
+        }
 
-      public SqlConnection Qcurso = new SqlConnection(@"Data Source=ACSERVER;Initial Catalog=QCurso;Persist Security Info=True;User ID=sa;Password=apcofo@21");
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    
+
+    // //Conexão com Banco de Dados de QCursos
+
+        public SqlConnection Qcurso = new SqlConnection(@"Data Source=ACSERVER;Initial Catalog=QCurso;Persist Security Info=True;User ID=sa;Password=apcofo@21");
 
         //Conexão com Banco de Dados de Cameras
        public  SqlConnection Cameras = new SqlConnection(@"Data Source=ACSERVER;Initial Catalog=GIG_Cameras;Persist Security Info=True;User ID=sa;Password=apcofo@21");
